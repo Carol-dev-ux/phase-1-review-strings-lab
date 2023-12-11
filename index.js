c0c1dc8694a4c6c4ca932b1a4ed5395756fdd50f
Caroline Wangari <karehwachira@gmail.com>
2023-12-11 18:13:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Carol"; 
+
+let welcomeMessage = `Welcome to Flatbook, ${"Carol"}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${"CAROL"}!`;
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
